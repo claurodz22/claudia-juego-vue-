ac0d8bb910d2b85a56b5871272764ff2570c7d27
claudia <claudiaelena091@gmail.com>
2025-03-16 13:03:36 -0400

    v-for para visualizar title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,44 @@
 <script setup>
 import LayoutHero from './components/Layout/LayoutHero.vue';
+import GameCard from './components/Games/GameCard.vue';
+import { onMounted, reactive } from 'vue'
+
+// objetos reactivos --> manejar arrays en vue de manera
+// reactiva y profunda, se utilizan para detectar cambios
+// ejmp, en la response de la API
+
+const API_URL = "https://gamestreamapi.herokuapp.com/api/games"
+
+const state = reactive ({
+  error: null, 
+  isLoading: false,
+  datos: []
+})
+
+const fetchGames =  async () =>{
+  try{
+    state.isLoading = true
+    const response = await fetch(API_URL)
+    const json = await response.json()
+    console.log(json)
+    state.datos = json
+  } catch(error){
+    console.error(error)
+    state.error = error
+  } finally{
+    state.isLoading = false
+  }
+}
+
+onMounted(() =>{
+  fetchGames()
+})
+
 </script>
 
 <template>
-  <LayoutHero />
+  <LayoutHero/>
+  <GameCard v-for="game in state.datos" :key="game.title" :game="game"/> 
   <main></main>
 </template>
 
